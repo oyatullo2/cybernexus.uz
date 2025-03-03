70556f9a2aeb3828f37f6d69e99f0b61f595986d
Oyatullo <izzatullayevoyatullo08@gmail.com>
2025-03-03 15:04:55 +0500

    Admins

--- src/Pages/Welcome/welcome.jsx
+++ src/Pages/Welcome/welcome.jsx
@@ -5,6 +5,9 @@ export const Welcome = () => {
         <div className="w-full flex flex-col items-center">
           <p>Hayrli kun !</p>
           <p>Bu saytda Kiber xavfsizlik bo'yicha malumotlar yangiliklar va yordam bo'limlari tez orada tashkil etiladi yangiliklardan xabardor bo'ling !</p>
+          <p>Malumot va murojaatlar uchun <a href="https://t.me/snovden_01">Admin 1</a></p>
+          <p>Malumot va murojaatlar uchun <a href="https://t.me/cyber01_guardians">Admin 2</a></p>
+          <p>Malumot va murojaatlar uchun <a href="https://t.me/umezhan">Admin 3</a></p>
         </div>
       </div>
     </>
